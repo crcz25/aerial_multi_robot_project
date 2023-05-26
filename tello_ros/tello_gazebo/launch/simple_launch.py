0630b1131a5ae4a2568211037d28d72ecee63e70
Carlos Cueto <crcueto25@gmail.com>
2023-05-26 20:16:27 +0300

    add fixed path for worlds

--- tello_ros/tello_gazebo/launch/simple_launch.py
+++ tello_ros/tello_gazebo/launch/simple_launch.py
@@ -10,8 +10,8 @@ from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     ns = 'drone1'
-    world_path = os.path.join(get_package_share_directory('tello_gazebo'), 'worlds', 'simple.world')
-    world_path = '/media/crcz/AR_MRS_Worlds03/04.world'
+    # world_path = os.path.join(get_package_share_directory('tello_gazebo'), 'worlds', 'simple.world')
+    world_path = '/media/crcz/AR_MRS_Worlds03/08.world'
     urdf_path = os.path.join(get_package_share_directory('tello_description'), 'urdf', 'tello_1.urdf')
 
     return LaunchDescription([
